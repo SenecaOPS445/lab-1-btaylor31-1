2f0cf754fcad9c21a2937f4c078d5fef9a6bb447
Bakari Taylor <btaylor31@myseneca.ca>
2025-01-13 23:33:06 -0500

    Test script to check understanding of concepts

---
+++ test4.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+name = 'Bakari'
+
+age = '20'
+
+print('Today I met ' + name + ' and I think he is older than ' + str(age) + ' years old by guessing, but what do you think?')
