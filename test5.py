eaa1a78df9da2ca83b0b7c92b455b0ddeec74f62
Bakari Taylor <btaylor31@myseneca.ca>
2025-01-13 23:39:29 -0500

    Another test of concepts

---
+++ test5.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+x = 5
+y = 50
+z = 90
+
+sum = z + x - y
+
+print('I have done the calculations and the answer is' + str(sum))
+print ('Let me know what you think. if you disagree enter and answer below')
+input("Enter a number = ")
+      
